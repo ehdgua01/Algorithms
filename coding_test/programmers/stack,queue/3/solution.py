ae71229c7ea517e28707d0a6de59f2a16bc1fcc3
KDH <ehdgua01@naver.com>
2019-12-03 00:01:24 +0900

    TIL: ADD - 코딩 테스트(프로그래머스-스탭,큐)

---
+++ coding_test/programmers/stack,queue/3/solution.py
@@ -0,0 +1,21 @@
+"""
+프로그래머스 알고리즘 문제
+
+https://programmers.co.kr/learn/courses/30/lessons/42588
+"""
+
+
+def solution(heights: list):
+    answer = []
+    heights.reverse()
+
+    for idx, height in enumerate(heights, start=1):
+        receiver = 0
+
+        for i, h in enumerate(heights[idx:]):
+            if h > height:
+                receiver = len(heights) - i - idx
+                break
+
+        answer.insert(0, receiver)
+    return answer
