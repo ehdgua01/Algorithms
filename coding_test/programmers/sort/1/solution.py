fadaad08efe9ddabbe7b0457f21e9d4867627a72
KDH <ehdgua01@naver.com>
2019-12-03 23:46:33 +0900

    TIL: ADD - 코딩 테스트(프로그래머스-정렬)

---
+++ coding_test/programmers/sort/1/solution.py
@@ -0,0 +1,12 @@
+"""
+프로그래머스 알고리즘 문제
+
+https://programmers.co.kr/learn/courses/30/lessons/42748
+"""
+
+
+def solution(array, commands):
+    return [
+        sorted(array[command[0]-1:command[1]])[command[2]-1]
+        for command in commands
+    ]
---
+++ coding_test/programmers/sort/1/test.py
@@ -0,0 +1,17 @@
+import unittest
+from solution import solution
+
+
+class TestCase(unittest.TestCase):
+    def test_solution(self):
+        self.assertEqual(
+            solution(
+                array=[1, 5, 2, 6, 3, 7, 4],
+                commands=[[2, 5, 3], [4, 4, 1], [1, 7, 3]],
+            ),
+            [5, 6, 3],
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
