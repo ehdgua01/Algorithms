0f681f786d9ff19abda59e43f4b641aaa5f20465
KDH <ehdgua01@naver.com>
2019-12-11 17:28:26 +0900

    TIL: UPDATE - 코딩 테스트(프로그래머스-스탭,큐)(효율성 테스트 성공)

--- coding_test/programmers/stack,queue/4/solution.py
+++ coding_test/programmers/stack,queue/4/solution.py
@@ -3,16 +3,20 @@
 
 https://programmers.co.kr/learn/courses/30/lessons/42584
 """
+from collections import deque
 
 
 def solution(prices: list):
     answer = []
+    prices = deque(prices)
 
-    for idx, price in enumerate(prices, start=1):
+    while prices:
+        price = prices.popleft()
         count = 0
-        for p in prices[idx:]:
+        for p in prices:
             count += 1
-            if p < price:
+            if price > p:
                 break
         answer.append(count)
+    answer[-1] = 0
     return answer
