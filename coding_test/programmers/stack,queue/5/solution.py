d1a05a0dca16c691ac11eba5b562e94be68fca70
KDH <ehdgua01@naver.com>
2019-12-15 20:38:52 +0900

    TIL: ADD - 코딩 테스트(프로그래머스-스탭,큐) 테스트 케이스 추가

---
+++ coding_test/programmers/stack,queue/5/solution.py
@@ -0,0 +1,10 @@
+"""
+프로그래머스 알고리즘 문제
+
+https://programmers.co.kr/learn/courses/30/lessons/42583
+"""
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
---
+++ coding_test/programmers/stack,queue/5/test.py
@@ -0,0 +1,36 @@
+import unittest
+from solution import solution
+
+
+class UnitTest(unittest.TestCase):
+    def test_solution(self):
+        self.assertEqual(
+            solution(
+                bridge_length=2,
+                weight=10,
+                truck_weights=[7, 4, 5, 6],
+            ),
+            8
+        )
+        self.assertEqual(
+            solution(
+                bridge_length=100,
+                weight=100,
+                truck_weights=[10],
+            ),
+            101,
+        )
+        self.assertEqual(
+            solution(
+                bridge_length=100,
+                weight=100,
+                truck_weights=[
+                    10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+                ],
+            ),
+            110,
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
