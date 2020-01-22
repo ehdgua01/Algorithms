b128340bd192b8790bd0decd58b8f0da37a43b6a
KDH <ehdgua01@naver.com>
2020-01-23 00:05:27 +0900

    singly linked list 리팩토링

---
+++ algorithms/data_structure/linked_list/sll/singly_linked_list.py
@@ -0,0 +1,81 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def append(self, new: Node):
+        if self.is_empty:
+            self.head = new
+            self.tail = new
+        else:
+            self.tail.next = new
+            self.tail = new
+        self.size += 1
+
+    def insert(self, new: Node, location: int):
+        if location < 1 or (self.is_empty and location != 1):
+            raise ValueError('invalid location')
+
+        if self.is_empty:
+            self.head = new
+            self.tail = new
+        else:
+            target = self.get_node_at(location)
+            new.next = target.next
+            if target == self.tail:
+                self.tail = new
+            target.next = new
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty:
+            raise ValueError('List is empty')
+        new_tail = self.get_node_at(self.size - 1)
+        new_tail.next = None
+        self.tail = new_tail
+        self.size -= 1
+
+    def remove(self, location: int):
+        if self.is_empty:
+            raise ValueError('List is empty')
+        if self.size < location:
+            raise ValueError('Invalid location')
+        if self.size == 1:
+            self.__init__()
+            return
+
+        target = self.head
+
+        if location == 1:
+            self.head = target.next
+            return
+        else:
+            location -= 1
+            while target.next and location - 1:
+                target = target.next
+                location -= 1
+            target.next = target.next.next
+        self.size -= 1
+
+    def get_node_at(self, location: int) -> Node:
+        if (location < 1) or self.size < location:
+            raise ValueError('Invalid location')
+        temp = self.head
+        while (temp and temp.next) and location - 1:
+            temp = temp.next
+            location -= 1
+        return temp
+
+    def get_data_at(self, location: int):
+        return self.get_node_at(location).data
+
+    @property
+    def is_empty(self):
+        return True if not self.size else False
---
+++ algorithms/data_structure/linked_list/sll/test.py
@@ -0,0 +1,87 @@
+import unittest
+from singly_linked_list import SinglyLinkedList, Node
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sll = SinglyLinkedList()
+
+    def test_append(self):
+        self.sll.append(Node(1))
+        self.assertEqual(self.sll.head.data, 1)
+        self.assertEqual(self.sll.head.next, None)
+        self.assertEqual(self.sll.tail.data, 1)
+        self.assertEqual(self.sll.tail.next, None)
+        self.assertEqual(self.sll.size, 1)
+
+        self.sll.append(Node(2))
+        self.assertEqual(self.sll.head.data, 1)
+        self.assertEqual(self.sll.head.next.data, 2)
+        self.assertEqual(self.sll.tail.data, 2)
+        self.assertEqual(self.sll.tail.next, None)
+        self.assertEqual(self.sll.size, 2)
+
+        self.sll.append(Node(3))
+        self.assertEqual(self.sll.head.data, 1)
+        self.assertEqual(self.sll.head.next.data, 2)
+        self.assertEqual(self.sll.head.next.next.data, 3)
+        self.assertEqual(self.sll.tail.data, 3)
+        self.assertEqual(self.sll.tail.next, None)
+        self.assertEqual(self.sll.size, 3)
+
+    def test_get_data_at(self):
+        self.assertRaises(ValueError, self.sll.get_data_at, location=1)
+        for i in range(1, 11):
+            self.sll.append(Node(i))
+        self.assertEqual(self.sll.get_data_at(1), 1)
+        self.assertEqual(self.sll.get_data_at(4), 4)
+        self.assertEqual(self.sll.get_data_at(9), 9)
+
+    def test_insert(self):
+        self.sll.insert(Node(1), 1)
+        self.assertEqual(self.sll.size, 1)
+        self.assertEqual(self.sll.head.data, 1)
+        self.assertEqual(self.sll.tail.data, 1)
+
+        self.sll.insert(Node(2), 1)
+        self.assertEqual(self.sll.size, 2)
+        self.assertEqual(self.sll.get_data_at(2), 2)
+        self.assertEqual(self.sll.tail.data, 2)
+
+        self.sll.insert(Node(3), 1)
+        self.assertEqual(self.sll.size, 3)
+        self.assertEqual(self.sll.get_data_at(2), 3)
+        self.assertEqual(self.sll.tail.data, 2)
+
+        self.sll.insert(Node(4), 3)
+        self.assertEqual(self.sll.size, 4)
+        self.assertEqual(self.sll.get_data_at(3), 2)
+        self.assertEqual(self.sll.get_data_at(4), 4)
+        self.assertEqual(self.sll.tail.data, 4)
+
+    def test_pop(self):
+        self.assertRaises(ValueError, self.sll.pop)
+        for i in range(1, 11):
+            self.sll.append(Node(i))
+        self.sll.pop()
+        self.assertEqual(self.sll.tail.data, 9)
+        self.assertEqual(self.sll.size, 9)
+        self.sll.pop()
+        self.assertEqual(self.sll.tail.data, 8)
+        self.assertEqual(self.sll.size, 8)
+
+    def test_remove(self):
+        self.assertRaises(ValueError, self.sll.remove, location=1)
+        for i in range(1, 11):
+            self.sll.append(Node(i))
+        self.sll.remove(7)
+        self.assertEqual(self.sll.get_data_at(7), 8)
+        self.assertEqual(self.sll.get_data_at(8), 9)
+        self.assertEqual(self.sll.size, 9)
+        self.sll.remove(3)
+        self.assertEqual(self.sll.get_data_at(3), 4)
+        self.assertEqual(self.sll.size, 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
