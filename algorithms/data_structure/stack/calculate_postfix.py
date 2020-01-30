4f3b44171c4ef1224f6679e0591f2858e1748092
KDH <ehdgua01@naver.com>
2020-01-31 02:45:52 +0900

    후위 표현식 계산기 구현

---
+++ algorithms/data_structure/stack/calculate_postfix.py
@@ -0,0 +1,32 @@
+import operator
+
+from stack import Stack, Node
+from infix_to_postfix import infix_to_postfix, is_number
+
+__OPERATOR = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.floordiv
+}
+
+
+def calculate_postfix(postfix: list):
+    result_stack = Stack()
+
+    for token in postfix:
+        try:
+            try:
+                result_stack.push(Node(int(token)))
+            except ValueError:
+                """10진수 정수가 아닌 경우
+                """
+                result_stack.push(Node(float(token)))
+        except ValueError:
+            if token in __OPERATOR:
+                p1 = result_stack.pop()
+                p2 = result_stack.pop()
+                result_stack.push(Node(__OPERATOR[token](p1, p2)))
+            else:
+                raise Exception('허용되지 않은 데이터')
+    return result_stack.pop()
