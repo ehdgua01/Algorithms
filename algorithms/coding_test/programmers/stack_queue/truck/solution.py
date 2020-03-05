ae77fa92407fd82f9ece3a5aa401017c5abc1057
KDH <ehdgua01@naver.com>
2020-03-05 20:59:18 +0900

    프로그래머스 트럭 코딩 테스트

---
+++ algorithms/coding_test/programmers/stack_queue/truck/solution.py
@@ -0,0 +1,26 @@
+"""
+프로그래머스 알고리즘 문제
+
+https://programmers.co.kr/learn/courses/30/lessons/42583
+"""
+from collections import deque
+
+
+def solution(bridge_length, weight, truck_weights: list):
+    answer = 0
+    elapsed = 0
+    truck_weights = deque(truck_weights)
+    on_bridge = deque()
+    entered = deque()
+
+    while truck_weights or entered:
+        elapsed += 1
+
+        if entered and (elapsed - entered[0] == bridge_length):
+            answer = bridge_length + entered.popleft()
+            on_bridge.popleft()
+
+        if truck_weights and ((sum(on_bridge) + truck_weights[0]) <= weight):
+            on_bridge.append(truck_weights.popleft())
+            entered.append(elapsed)
+    return answer
