d6ff19e1a9876273577654199de4f04bfec0e9f9
KDH <ehdgua01@naver.com>
2020-03-06 21:39:19 +0900

    정렬 되었는 지 체크하여 성능 개선

--- algorithms/sort/bubble_sort/bubble_sort.py
+++ algorithms/sort/bubble_sort/bubble_sort.py
@@ -1,8 +1,12 @@
 def bubble_sort(data: list) -> list:
     for i in range(len(data) - 1):
+        swapped = False
+
         for j in range(len(data) - (i + 1)):
             if data[j] > data[j + 1]:
-                temp = data[j]
-                data[j] = data[j + 1]
-                data[j + 1] = temp
+                swapped = True
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+        if not swapped:
+            break
     return data
