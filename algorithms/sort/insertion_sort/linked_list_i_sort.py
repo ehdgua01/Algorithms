fd1d24b662efd29aba761692b9f6d6f067c23c21
KDH <ehdgua01@naver.com>
2020-03-08 23:37:47 +0900

    링크드 리스트를 이용한 삽입 정렬 알고리즘 구현

---
+++ algorithms/sort/insertion_sort/linked_list_i_sort.py
@@ -0,0 +1,55 @@
+class Node(object):
+    def __init__(self, value: int) -> None:
+        self.prev = None
+        self.next = None
+        self.value = value
+
+    def __iter__(self):
+        return NodeIterator(self)
+
+    def insert(self, node) -> None:
+        if self.next:
+            node.next = self.next
+            self.next.prev = node
+        else:
+            node.next = None
+        self.next = node
+        node.prev = self
+
+    def pop(self):
+        if self.next:
+            self.next.prev = self.prev
+        if self.prev:
+            self.prev.next = self.next
+        return self
+
+
+class NodeIterator(object):
+    def __init__(self, node: Node):
+        self.current: Node = node
+
+    def __iter__(self) -> Node:
+        return self.current
+
+    def __next__(self) -> Node:
+        if self.current:
+            current = self.current
+            self.current = self.current.next
+            return current
+        raise StopIteration
+
+
+def linked_list_i_sort(data: Node) -> Node:
+    if data.next is None:
+        return data
+
+    i = data
+    while i:
+        if (i.prev is None) or (i.prev.value <= i.value):
+            i = i.next
+            continue
+        for j in data:
+            if i.value < j.value:
+                j.prev.insert(i.pop())
+                break
+        i = i.next
--- algorithms/sort/insertion_sort/test.py
+++ algorithms/sort/insertion_sort/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from .insertion_sort import insertion_sort
+from .linked_list_i_sort import linked_list_i_sort, Node
 
 
 class TestCase(unittest.TestCase):
@@ -17,3 +18,24 @@ class TestCase(unittest.TestCase):
             insertion_sort([1, 1, 2, 1, 2, 1, 1, 2, 1]),
             [1, 1, 1, 1, 1, 1, 2, 2, 2],
         )
+
+    def test_linked_list_i_sort(self):
+        unsorted = Node(1)
+        unsorted.insert(Node(4))
+        unsorted.insert(Node(6))
+        unsorted.insert(Node(3))
+        unsorted.insert(Node(5))
+        unsorted.insert(Node(2))
+        unsorted.insert(Node(8))
+        unsorted.insert(Node(7))
+        unsorted.insert(Node(9))
+
+        linked_list_i_sort(unsorted)
+
+        for i in range(1, 10):
+            j = 1
+            v = unsorted
+            while j < i:
+                v = v.next
+                j += 1
+            self.assertEqual(v.value, i)
