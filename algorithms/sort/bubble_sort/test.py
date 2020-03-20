2180c0006e64db8d72445c3a873a8c8f52101edf
KDH <ehdgua01@naver.com>
2020-03-20 19:44:57 +0900

    Fix remove function

--- algorithms/sort/bubble_sort/test.py
+++ algorithms/sort/bubble_sort/test.py
@@ -1,14 +1,10 @@
 import unittest
 
-from .bubble_sort import is_sorted, bubble_sort
+from .bubble_sort import bubble_sort
 
 
 class TestCase(unittest.TestCase):
     def test_bubble_sort(self):
-        self.assertEqual(
-            is_sorted([1, 2, 3, 4, 5], 5), True,
-        )
-
         self.assertEqual(
             bubble_sort([8, 9, 4, 1, 5, 6, 7, 2, 3]), [1, 2, 3, 4, 5, 6, 7, 8, 9],
         )
