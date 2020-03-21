553fcbdcafd0ab9c2c341d9afde5ce766c7f0948
KDH <ehdgua01@naver.com>
2020-03-21 19:10:34 +0900

    ADD matrix fibonacci algorithms unit test

--- algorithms/maths/fibonacci/test.py
+++ algorithms/maths/fibonacci/test.py
@@ -1,8 +1,12 @@
 import unittest
 
 from .fibonacci import fibonacci
+from .matrix_fibonacci import matrix_fibonacci
 
 
 class TestCase(unittest.TestCase):
     def test_fibonacci(self):
         self.assertEqual(fibonacci(8), 21)
+
+    def test_matrix_fibonacci(self):
+        self.assertEqual(matrix_fibonacci(100), 354224848179261915075)
