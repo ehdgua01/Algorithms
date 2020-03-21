96a33797631da3bb8a283cce84f76844ef05ec54
KDH <ehdgua01@naver.com>
2020-03-21 20:35:23 +0900

    ADD simple longest common subsequence

---
+++ algorithms/dynamic_planning/longest_common_subsequence/test.py
@@ -0,0 +1,26 @@
+import unittest
+from typing import List
+
+from .simple_lcs import simple_lcs
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.first_string: str = "GOOD MORNING"
+        self.second_string: str = "GUTEN MORGEN"
+        self.result_matrix: List[List[int]] = [
+            [0] * (len(self.second_string) + 1)
+            for _ in range(len(self.first_string) + 1)
+        ]
+
+    def test_simple_lcs(self) -> None:
+        self.assertEqual(
+            simple_lcs(
+                self.first_string,
+                self.second_string,
+                len(self.first_string),
+                len(self.second_string),
+                self.result_matrix,
+            ),
+            6,
+        )
