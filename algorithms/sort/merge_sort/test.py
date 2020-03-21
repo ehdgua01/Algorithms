095b62b09121c36cb03053b87870b48f0c34fba2
KDH <ehdgua01@naver.com>
2020-03-21 19:08:40 +0900

    ADD merge sort unit test

---
+++ algorithms/sort/merge_sort/test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from .merge_sort import merge_sort
+
+
+class TestCase(unittest.TestCase):
+    def test_merge_sort(self):
+        data = [0, 2, 5, 9, 3, 7, 4, 6, 8, 1]
+        self.assertEqual(merge_sort(data, 0, len(data)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
