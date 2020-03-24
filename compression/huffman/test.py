fd788d27d1ddb275fcf79ea216388696619c9e03
KDH <ehdgua01@naver.com>
2020-03-24 17:14:02 +0900

    ADD result type

--- compression/huffman/test.py
+++ compression/huffman/test.py
@@ -4,13 +4,13 @@ from .huffman import huffman_encode, huffman_decode
 
 
 class TestCase(unittest.TestCase):
-    def test_huffman(self):
+    def test_huffman(self) -> None:
         encoded, prefix_tree = huffman_encode("aaabbaaccdeaf")
         self.assertEqual(encoded, "00011111100101101110010001101")
         decoded = huffman_decode(encoded, prefix_tree)
         self.assertEqual(decoded, "aaabbaaccdeaf")
 
-    def test_same_value_data(self):
+    def test_same_value_data(self) -> None:
         encoded, prefix_tree = huffman_encode("ppppppppppp")
         self.assertEqual(encoded, "00000000000")
         decoded = huffman_decode(encoded, prefix_tree)
