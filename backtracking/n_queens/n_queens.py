0313c3fdf1c7d013fbb22e8d80e209cf409aa082
KDH <ehdgua01@naver.com>
2020-03-25 17:28:53 +0900

    UPDATE variable name(table => board)

--- backtracking/n_queens/n_queens.py
+++ backtracking/n_queens/n_queens.py
@@ -1,39 +1,39 @@
 from typing import List, Union, Tuple
 
 RESULT_TYPE = List[List[Tuple[int, int]]]
-TABLE_TYPE = List[Union[int, None]]
+BOARD_TYPE = List[Union[int, None]]
 
 
-def is_safe(table: TABLE_TYPE, row: int) -> bool:
+def is_safe(board: BOARD_TYPE, row: int) -> bool:
     current_row = 0
 
     while current_row < row:
-        if table[current_row] == table[row] or (
-            abs(table[current_row] - table[row]) == abs(current_row - row)
+        if board[current_row] == board[row] or (
+            abs(board[current_row] - board[row]) == abs(current_row - row)
         ):
             return False
         current_row += 1
     return True
 
 
-def find_queen(row: int, table: TABLE_TYPE, result: RESULT_TYPE) -> None:
-    if not is_safe(table, row):
+def find_queen(row: int, board: BOARD_TYPE, result: RESULT_TYPE) -> None:
+    if not is_safe(board, row):
         return
 
-    if row == len(table) - 1:
-        result.append([(x, y) for y, x in enumerate(table) if x is not None])
+    if row == len(board) - 1:
+        result.append([(x, y) for y, x in enumerate(board) if x is not None])
     else:
-        for i in range(len(table)):
-            table[row + 1] = i
-            find_queen(row + 1, table, result)
+        for i in range(len(board)):
+            board[row + 1] = i
+            find_queen(row + 1, board, result)
 
 
 def n_queens(rows: int) -> RESULT_TYPE:
     result: RESULT_TYPE = []
-    table: TABLE_TYPE = [None] * rows
+    board: BOARD_TYPE = [None] * rows
 
     for i in range(rows):
-        table[0] = i
-        find_queen(0, table, result)
+        board[0] = i
+        find_queen(0, board, result)
 
     return result
