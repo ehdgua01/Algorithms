cb73b25acb2ce46f0a07b47516a85905adc5c8fb
KDH <ehdgua01@naver.com>
2020-04-10 19:29:28 +0900

    ADD Programmers algorithm(number_baseball)

---
+++ coding_test/programmers/brute_force/number_baseball/solution.py
@@ -0,0 +1,30 @@
+"""
+프로그래머스 알고리즘 문제
+
+https://programmers.co.kr/learn/courses/30/lessons/42841
+"""
+from typing import List
+from itertools import permutations
+
+
+def solution(baseball: List[List[int]]) -> int:
+    answer = list(permutations(["1", "2", "3", "4", "5", "6", "7", "8", "9"], 3))
+
+    for q in baseball:
+        if q[1] == 3:
+            return 1
+
+        str_ = str(q[0])
+
+        for a in answer[:]:
+            S = 0
+
+            for i in range(3):
+                if str_[i] == a[i]:
+                    S += 1
+
+            B = len(set(str_).intersection(a)) - S
+
+            if S != q[1] or B != q[2]:
+                answer.remove(a)
+    return len(answer)
---
+++ coding_test/programmers/brute_force/number_baseball/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from .solution import solution
+
+
+class TestCase(unittest.TestCase):
+    def test_case_1(self) -> None:
+        self.assertEqual(
+            solution([[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]), 2
+        )
