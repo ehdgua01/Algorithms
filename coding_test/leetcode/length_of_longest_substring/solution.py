aa2e15242e77e2f4d716df918e4d0526881b0741
KDH <ehdgua01@naver.com>
2020-04-24 12:52:28 +0900

    FIX coding convention error

--- coding_test/leetcode/length_of_longest_substring/solution.py
+++ coding_test/leetcode/length_of_longest_substring/solution.py
@@ -5,7 +5,7 @@ class Solution:
 
         for x in s:
             if x in temp_s:
-                temp_s = temp_s[temp_s.index(x) + 1:]
+                temp_s = temp_s[temp_s.index(x) + 1 :]
             temp_s += x
             answer = answer if len(temp_s) < answer else len(temp_s)
         return answer
