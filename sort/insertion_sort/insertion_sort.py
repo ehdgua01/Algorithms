40998ea529c592cca52899801fb24993b0c0f2a4
KDH <ehdgua01@naver.com>
2020-04-25 20:56:55 +0900

    UPDATE insertion sort syntax

--- sort/insertion_sort/insertion_sort.py
+++ sort/insertion_sort/insertion_sort.py
@@ -5,6 +5,6 @@ def insertion_sort(data: list) -> list:
 
         swapped = i
         while swapped > 0 and data[swapped] < data[swapped - 1]:
-            data[swapped - 1], data[swapped] = (data[swapped], data[swapped - 1])
+            data[swapped - 1], data[swapped] = data[swapped], data[swapped - 1]
             swapped -= 1
     return data
