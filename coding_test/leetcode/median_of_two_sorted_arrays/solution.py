8321025dd5f0d485f8f7a5d85367edd2690e413a
KDH <ehdgua01@naver.com>
2020-04-30 03:37:31 +0900

    UPDATE optimize with merge sort

--- coding_test/leetcode/median_of_two_sorted_arrays/solution.py
+++ coding_test/leetcode/median_of_two_sorted_arrays/solution.py
@@ -3,6 +3,21 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1 = sorted(nums1 + nums2)
-        q, r = divmod(len(nums1), 2)
-        return nums1[q] if r else (nums1[q] + nums1[q - 1]) / 2
+        a, b = 0, 0
+        i, j, h = 0, 0, (len(nums1) + len(nums2)) // 2
+
+        while i + j <= h:
+            if i < len(nums1) and j < len(nums2):
+                if nums1[i] < nums2[j]:
+                    a, b = b, nums1[i]
+                    i += 1
+                else:
+                    a, b = b, nums2[j]
+                    j += 1
+            elif i < len(nums1):
+                a, b = b, nums1[i]
+                i += 1
+            else:
+                a, b = b, nums2[j]
+                j += 1
+        return b if (len(nums1) + len(nums2)) % 2 == 1 else (a + b) / 2
