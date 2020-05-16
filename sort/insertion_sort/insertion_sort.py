dc67fd7196487542084d3ec3c8e464eb01a187dc
KDH <ehdgua01@naver.com>
2020-05-16 18:47:42 +0900

    UPDATE insertion sort

--- sort/insertion_sort/insertion_sort.py
+++ sort/insertion_sort/insertion_sort.py
@@ -1,8 +1,5 @@
 def insertion_sort(data: list) -> list:
     for i in range(1, len(data)):
-        if data[i - 1] < data[i]:
-            continue
-
         swapped = i
         while swapped > 0 and data[swapped] < data[swapped - 1]:
             data[swapped - 1], data[swapped] = data[swapped], data[swapped - 1]
