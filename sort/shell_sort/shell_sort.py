6fac98f5d15a881443deb9522f824e15fe78bd2e
KDH <ehdgua01@naver.com>
2020-05-25 16:44:25 +0900

    UPDATE Enhance shell sort syntax

--- sort/shell_sort/shell_sort.py
+++ sort/shell_sort/shell_sort.py
@@ -6,12 +6,9 @@ def shell_sort(collection: List[Any]) -> List[Any]:
 
     while 0 < gap:
         for i in range(gap, len(collection)):
-            j, temp = i, collection[i]
-
-            while j >= gap and temp < collection[j - gap]:
-                collection[j] = collection[j - gap]
+            j = i
+            while j >= gap and collection[j] < collection[j - gap]:
+                collection[j], collection[j - gap] = collection[j - gap], collection[j]
                 j -= gap
-
-            collection[j] = temp
         gap //= 2
     return collection
