fce8bcb01e31853c2a88f53437d26b0934620f13
KDH <ehdgua01@naver.com>
2020-05-25 16:36:41 +0900

    UPDATE Enhance merge sort syntax

--- sort/merge_sort/merge_sort.py
+++ sort/merge_sort/merge_sort.py
@@ -1,16 +1,16 @@
-from typing import Union
+from typing import Union, List, Any
 
 
-def merge_sort(data: list, left: int, right: int) -> Union[list, None]:
+def merge_sort(data: List[Any], left: int, right: int) -> Union[List[Any], None]:
     if (right - left) < 2:
         return
 
     a = left
     b = middle = (right + left) // 2
+    temp = []
+
     merge_sort(data, left, middle)
     merge_sort(data, middle, right)
-
-    temp = []
 
     while a < middle and b < right:
         if data[a] <= data[b]:
@@ -21,13 +21,9 @@ def merge_sort(data: list, left: int, right: int) -> Union[list, None]:
             b += 1
 
     if a < middle:
-        while a < middle:
-            temp.append(data[a])
-            a += 1
+        temp += data[a:middle]
     else:
-        while b < right:
-            temp.append(data[b])
-            b += 1
+        temp += data[b:right]
 
     data[left:right] = temp
     return data
