02b3cf405e2c85e8117e126ceb144375c9fac315
KDH <ehdgua01@naver.com>
2020-07-14 10:30:22 +0900

    update anagram algorithm using Counter

--- others/anagram/anagram.py
+++ others/anagram/anagram.py
@@ -1,10 +1,6 @@
+from collections import Counter
+
+
 def anagram(s1: str, s2: str) -> bool:
     s1, s2 = s1.replace(" ", ""), s2.replace(" ", "")
-
-    if len(s1) != len(s2):
-        return False
-
-    if sorted(s1) != sorted(s2):
-        return False
-
-    return True
+    return Counter(s1) == Counter(s2)
