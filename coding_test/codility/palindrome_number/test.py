6ac442446da2ed5b9fdcc5589c426defcda55896
KDH <ehdgua01@naver.com>
2021-10-31 16:44:40 +0900

    테스트케이스 추가

--- coding_test/codility/palindrome_number/test.py
+++ coding_test/codility/palindrome_number/test.py
@@ -10,7 +10,15 @@ def solution():
 
 @pytest.mark.parametrize(
     "value,expected",
-    ((121, True), (-121, False), (10, False), (-101, False), (12321, True), (11, True)),
+    (
+        (121, True),
+        (-121, False),
+        (10, False),
+        (-101, False),
+        (12321, True),
+        (11, True),
+        (123, False),
+    ),
 )
 def test_solution(solution, value: int, expected: bool):
     assert solution(value) is expected
