34d573b2e4c45164536299d32d9fd6fe6b5351a0
KDH <ehdgua01@naver.com>
2021-10-31 16:35:32 +0900

    문자열로 바꾸지 않고 구현

--- coding_test/codility/palindrome_number/solution.py
+++ coding_test/codility/palindrome_number/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return False if x < 10 else x == int(str(x)[::-1])
+        # return False if x < 10 else x == int(str(x)[::-1])
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reversed_x = 0
+        while reversed_x < x:
+            reversed_x = reversed_x * 10 + x % 10
+            x //= 10
+        return x == reversed_x or x == reversed_x // 10
