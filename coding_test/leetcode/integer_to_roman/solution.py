cd2ab90016bdaf1eb36ce66236ea37132a115fd6
KDH <ehdgua01@naver.com>
2021-12-01 08:55:07 +0900

    조금 더 개선

--- coding_test/leetcode/integer_to_roman/solution.py
+++ coding_test/leetcode/integer_to_roman/solution.py
@@ -1,22 +1,9 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = {
-            1: "I",
-            4: "IV",
-            5: "V",
-            9: "IX",
-            10: "X",
-            40: "XL",
-            50: "L",
-            90: "XC",
-            100: "C",
-            400: "CD",
-            500: "D",
-            900: "CM",
-            1000: "M",
-        }
+        keys = ("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+        values = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
         result = ""
-        for int_, roman_num in reversed(symbols.items()):
+        for roman_num, int_ in zip(keys, values):
             if num // int_ != 0:
                 s, num = divmod(num, int_)
                 result += roman_num * s
