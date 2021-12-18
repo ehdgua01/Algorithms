f1fdddfc8c46bfcde193993bcf207ddb50b4f440
KDH <ehdgua01@naver.com>
2021-12-18 12:16:16 +0900

    코드 개선

--- coding_test/leetcode/longest_common_prefx/solution.py
+++ coding_test/leetcode/longest_common_prefx/solution.py
@@ -3,12 +3,9 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs[0]:
-            return ""
-        prefix = strs[0][0]
-        while True:
-            if not all(s.startswith(prefix) for s in strs):
-                return prefix[:-1]
-            if len(prefix) == len(strs[0]):
-                return prefix
-            prefix += strs[0][len(prefix)]
+        i = 0
+        for s in zip(*strs):
+            if len(set(s)) != 1:
+                break
+            i += 1
+        return strs[0][:i]
