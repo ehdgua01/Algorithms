ef50735388511c1292ab13afab14e470f1678e64
KDH <ehdgua01@naver.com>
2024-09-10 08:57:07 +0900

    [프로그래머스] 더 맵게

---
+++ coding_test/programmers/heap/more_spicy.py
@@ -0,0 +1,28 @@
+import heapq
+
+
+def solution(scoville, K):
+    scoville.sort()
+    mixed = 0
+    while len(scoville) > 1 and scoville[0] < K:
+        first, second = heapq.heappop(scoville), heapq.heappop(scoville)
+        s = first + second * 2
+        heapq.heappush(scoville, s)
+        mixed += 1
+    return mixed if scoville[0] >= K else -1
+
+
+def test_cases():
+    assert solution([1, 2, 3, 9, 10, 12], 7) == 2
+    assert solution([1], 2) == -1
+    assert solution([2], 2) == 0
+    assert (
+        solution(
+            [
+                2,
+                3,
+            ],
+            2,
+        )
+        == 0
+    )
