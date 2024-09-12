76d7bc66af6b062f1978a7cd2c6d8c13602e5836
KDH <ehdgua01@naver.com>
2024-09-12 13:56:36 +0900

    [프로그래머스] 섬 연결하기

---
+++ coding_test/programmers/greedy/bridge_island.py
@@ -0,0 +1,41 @@
+def solution(n, costs):
+    costs.sort(key=lambda x: x[2])
+    tree = [-1] * n
+    cost = 0
+    for a, b, c in costs:
+        while tree[a] != -1:
+            a = tree[a]
+        while tree[b] != -1:
+            b = tree[b]
+        if a != b:
+            tree[b] = a
+            cost += c
+    return cost
+
+
+"""
+-1 -1 -1 -1
+-1  0 -1 -1
+-1  0  0 -1
+-1  0  0  0
+"""
+
+
+def test_cases():
+    assert solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]) == 4
+    assert solution(4, [[0, 1, 1], [1, 2, 5], [1, 3, 1], [2, 3, 8]]) == 7
+    assert (
+        solution(
+            5,
+            [
+                [0, 1, 5],
+                [2, 3, 3],
+                [1, 2, 3],
+                [3, 1, 2],
+                [3, 0, 4],
+                [2, 4, 6],
+                [4, 0, 7],
+            ],
+        )
+        == 15
+    )
