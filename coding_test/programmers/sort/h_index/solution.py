eaa19958cef796c47c1d329d52426f23a8d596cd
KDH <ehdgua01@naver.com>
2024-09-16 22:57:01 +0900

    [프로그래머스] H-index 재풀이

--- coding_test/programmers/sort/h_index/solution.py
+++ coding_test/programmers/sort/h_index/solution.py
@@ -4,15 +4,7 @@
 https://programmers.co.kr/learn/courses/30/lessons/42747
 """
 
-from typing import List
 
-
-def solution(citations: List[int]) -> int:
-    answer = len(citations)
+def solution(citations) -> int:
     citations.sort(reverse=True)
-
-    for i, c in enumerate(citations):
-        if c <= i:
-            answer = i
-            break
-    return answer
+    return next((i for i, c in enumerate(citations) if c <= i), len(citations))
