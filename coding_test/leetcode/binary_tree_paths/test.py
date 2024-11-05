073136db2193595a5d5e6177fae180651248fbd9
KDH <ehdgua01@naver.com>
2024-11-05 11:49:41 +0900

    [Leetcode] binary-tree-paths

---
+++ coding_test/leetcode/binary_tree_paths/test.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        result = []
+        stack = []
+
+        def backtrack(node: TreeNode):
+            stack.append(node.val)
+            if node.left is node.right is None:
+                result.append("->".join(map(str, stack)))
+                return
+            if node.left:
+                backtrack(node.left)
+                stack.pop()
+            if node.right:
+                backtrack(node.right)
+                stack.pop()
+
+        backtrack(root)
+        return result
+
+
+def test_binary_tree_paths():
+    s = Solution()
+    assert s.binaryTreePaths(TreeNode(1)) == ["1"]
+    assert s.binaryTreePaths(
+        TreeNode(
+            1,
+            left=TreeNode(
+                2,
+                right=TreeNode(5),
+            ),
+            right=TreeNode(3),
+        )
+    ) == ["1->2->5", "1->3"]
